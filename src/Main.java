9c97965c1a6a1afd3521c54de7263e197571d1a6
EvgenyGoryunov <89795937+EvgenyGoryunov@users.noreply.github.com>
2022-02-14 14:37:02 +0700

    5-добавлены комменты к коду 4-задание полностью готово 3-до внедрения цикла вывода сообщения правильного 2-запись в файл тест 1-сделаны рандомные выводы фраз из разных файлов

--- src/Main.java
+++ src/Main.java
@@ -1,219 +1,119 @@
-import java.awt.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Random;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         randMessages();
     }
 
-    public static void randMessages() throws FileNotFoundException {
+    public static void randMessages() throws IOException {
 
+//        создаем объект класса Файл
         File file = new File(".\\text");
 
+//        создаем массив listfiles, в который помещаем список файлов, содержащихся в директории ".\\text"
         String[] listfiles = file.list();
-        for (String files : listfiles) {
 
-            File pathFiles = new File(".\\text\\" + files);
+//        цикл для формирования сообщений на 7 дней (на неделю вперед)
+        for (int z = 0; z < 7; z++) {
 
-            Scanner jokeOfComp = new Scanner(new FileInputStream(pathFiles));
-            ArrayList<String> jokeOfCompList = new ArrayList<String>();
+//            цикл для формирования правильно сообщения
+            for (int i = 0; i < 2; i++) {
 
-            while (jokeOfComp.hasNextLine()) {
-                jokeOfCompList.add(jokeOfComp.nextLine());
+//                условие, при котором первой строчкой формируется пунктирная линия разделитель + дата
+                if (i == 0) {
+
+//                    создаем объект класса календарь
+                    Calendar calendar = new GregorianCalendar();
+//                    добавляет нашей дате при каждом цикле одно значение, чтоб было смещение по датам
+                    calendar.add(Calendar.DATE, z);
+//                    получить текущее значение даты и времени
+                    Date dateNow = calendar.getTime();
+
+//                    формирование правильно вывода даты (число, месяц, год)
+                    SimpleDateFormat formatter = new SimpleDateFormat("EEEE (dd-MMMM-YYYY)");
+                    String dateMessage = formatter.format(dateNow);
+
+//                    записать в файл output.txt сообщение в виде: 1-пунктирная линия, 2-дата
+                    FileWriter qwe = new FileWriter("./output.txt", true);
+//                    вписываем линию
+                    qwe.write("\n------------------------------------------------------------------------------------" + "\n");
+//                    вписываем дату
+                    qwe.write("дата: " + dateMessage + "\n");
+                    qwe.flush();
+
+//               условие, при котором прописывается фраза шутка дня
+                } else if (i == 1) {
+
+                    FileWriter messWrite = new FileWriter("./output.txt", true);
+                    messWrite.write(" ШУТКА" + "\n");
+                    messWrite.flush();
+
+                    int j = 0;
+
+//                    циклом выводим все фразы, по одной из каждого документа
+                    for (String files : listfiles) {
+
+                        if (j <= 1) {
+
+                            File pathFiles = new File(".\\text\\" + files);
+
+//                            создаем поток инфы из файла
+                            Scanner message = new Scanner(new FileInputStream(pathFiles));
+//                            создаем список, в который в дальшейшем поместим наши строчки с файлов
+                            ArrayList<String> messList = new ArrayList<String>();
+
+//                            через цикл while проходимся по каждой строчке сверху вниз и добавляем в наш список
+//                            каждую строчку
+                            while (message.hasNextLine()) {
+                                messList.add(message.nextLine());
+                            }
+
+//                            создаем объект класс рандом, для выбора из нашего списка случайной фразы
+                            Random randomMess = new Random();
+                            String rrr = messList.get(randomMess.nextInt(messList.size()));
+
+//                            выбранную случайную фразу прописываем в наш текстовый файл
+                            FileWriter qwe1 = new FileWriter("./output.txt", true);
+//                            формат вывода сообщения в файл
+                            messWrite.write((j + 1) + ". " + rrr + "\n");
+                            messWrite.flush();
+
+//                            условие, при котором вписывается фраза "мотивашки"
+                            if (j == 1) {
+                                messWrite.write(" МОТИВАЦИЯ " + "\n");
+                                messWrite.flush();
+                            }
+
+                            j += 1;
+
+//                            условие написания мотивашек (2шт)
+                        } else if (j > 1) {
+
+                            File pathFiles = new File(".\\text\\" + files);
+
+                            Scanner jokeOfComp = new Scanner(new FileInputStream(pathFiles));
+                            ArrayList<String> messageMes = new ArrayList<String>();
+
+                            while (jokeOfComp.hasNextLine()) {
+                                messageMes.add(jokeOfComp.nextLine());
+                            }
+
+                            Random randomMotiv = new Random();
+                            String mesMes = messageMes.get(randomMotiv.nextInt(messageMes.size()));
+
+                            messWrite.write((j - 1) + ". " + mesMes + "\n");
+                            messWrite.flush();
+
+                            j += 1;
+
+                        }
+                    }
+                }
             }
-
-            Random jokeOfCompRand = new Random();
-            System.out.println("\nрандом фраза из файла " + files + "\n" +  jokeOfCompList.get(jokeOfCompRand.nextInt(jokeOfCompList.size())));
-
         }
-
-//        FileWriter writer = new FileWriter("./text/output.txt", false);
-//        writer.write(resString.trim());
-//        writer.flush();
-
-
-
-
-
-
-
-//            System.out.println(files);
-//            System.out.println(pathFiles);
-//        Scanner jokeOfComp = new Scanner(new FileInputStream("./text/1comp.txt"));
-//        ArrayList<String> jokeOfCompList = new ArrayList<String>();
-//
-//        while (jokeOfComp.hasNextLine()) {
-//            jokeOfCompList.add(jokeOfComp.nextLine());
-//        }
-//
-//        Random jokeOfCompRand = new Random();
-//        System.out.println("\nрандом фраза1: " + jokeOfCompList.get(jokeOfCompRand.nextInt(jokeOfCompList.size())));
-//
-//
-//        Scanner aphorOfMerfi = new Scanner(new FileInputStream("./src/3merfi.txt"));
-//        ArrayList<String> aphorOfMerfiList = new ArrayList<String>();
-//
-//        while (aphorOfMerfi.hasNextLine()) {
-//            aphorOfMerfiList.add(aphorOfMerfi.nextLine());
-//        }
-//
-//        Random aphorOfMerfiRand = new Random();
-//        System.out.println("\nрандом фраза2: " + aphorOfMerfiList.get(aphorOfMerfiRand.nextInt(aphorOfMerfiList.size())));
-
     }
 }
-
-
-//        System.out.println("2");
-//        int qqq = 0;
-//            qqq += 1;
-//            System.out.println("счетчик: " + qqq);
-
-//            System.out.println("3");
-
-//        System.out.println("длина списка: " + list1.size());
-//        for (String qaz : list1) {
-//            System.out.println(qaz);
-//        }
-
-
-//        List test1 = new List();
-//            System.out.println(test.nextLine());
-//            list123.add(test.nextLine());
-
-
-//        while (test.hasNextLine()) {
-//
-//            System.out.println("3");
-//
-//            System.out.println(test.nextLine());
-//
-//            test1.add(test.nextLine());
-//
-//
-//            System.out.println("4");
-//
-////            System.out.println(test.nextLine());
-//
-//            }
-
-
-//        System.out.println("длина списка: " + test1.getSize());
-
-//        for (String qaz : test1) {
-//            System.out.println("4");
-//            System.out.print(qaz);
-//            System.out.println("5");
-
-//        }
-
-//            qqq += 1;
-//        из данного цикла получают переменную размер для нашего будущего массива
-
-
-//        int qqq = 0;
-//        while (test.hasNextLine()) {
-//            System.out.println(test.nextLine());
-//            qqq += 1;
-//        }
-//
-////        создаем массив для наших данных (предложения из файла)
-//        String[] www = new String[qqq];
-//        int i = 0;
-//
-//        System.out.println("1");
-//        while (test.hasNextLine()) {
-//            System.out.println("2");
-//            System.out.println(i);
-//            www[i] = test.nextLine();
-//            System.out.println(test.nextLine());
-//            i +=1;
-//            System.out.println(i);
-//
-//        }
-
-
-//        System.out.println("\n\nпеременная qqq: " + qqq);
-
-//        System.out.println("1");
-//            System.out.println("2-1");
-//            System.out.println(qqq);
-
-//            test.close();
-
-//            System.out.println("\nразмер массива: " + fromcomp.length);
-//            for (String qaz : fromcomp) {
-////                System.out.println("4");
-//                System.out.print(qaz + " ");
-////                System.out.println("5");
-//            }
-//            String[] fromcomp = test.nextLine().split(" ");
-
-//            System.out.println("3");
-
-//        int qwe = 0;
-//        while (test.hasNextLine()) {
-//            qwe +=1;
-//            System.out.println(qwe);
-//        }
-
-//        System.out.println(qwe);
-
-//        System.out.println("3");
-//
-//
-//        while (test.hasNextLine()) {
-//
-//            String[] fromcomp;
-//
-//
-//
-//            String qaz = test.nextLine();
-//            System.out.println(qaz);
-////            System.out.println("3");
-//        }
-//
-//        System.out.println("4");
-//    }
-//}
-//        while (test.hasNextLine()) {
-//            System.out.println("3");
-////            System.out.println(comp.hasNext());
-////            System.out.println(test.hasNextLine());
-//        }
-
-
-//        comp.close();
-
-//        String[] fromcomp = comp.nextLine().split(".");
-//        String[] fromcomp = comp.nextLine().split(".");
-
-
-//        for (String qaz : fromcomp) {
-//            System.out.println("4");
-//            System.out.print(qaz);
-//            System.out.println("5");
-//        }
-
-
-//        проходит 3 раза
-//        while (comp.hasNextLine()) {
-
-//            System.out.println("3");
-
-//            String[] fromcomp = comp.nextLine().split(".");
-//
-//            System.out.println("4");
-//            System.out.print(fromcomp);
-//            System.out.println("5");
-
-
-//        }
-//        return qaz;
